8a1c88954b0a41fbacc3dbea466ab53f712a02bd
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-12-21 18:37:42 -0300

    feat: add transaction type badge

---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,31 @@
+import { Badge } from "@/app/_components/ui/badge"
+import { Transaction, TransactionType } from "@prisma/client"
+import { Circle } from "lucide-react"
+
+interface TransactionTypeBadgeProps {
+    transaction: Transaction
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className=" hover:bg-muted bg-muted font-bold text-primary">
+        <Circle size={10}  className="mr-2 fill-primary"/>
+        Depósito
+      </Badge>)
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="font-bold text-danger bg-danger bg-opacity-10">
+        <Circle size={10}  className="mr-2 fill-danger"/>
+        Despesa
+      </Badge>)
+  }
+  return (
+    <Badge className="font-bold text-white bg-white bg-opacity-10">
+      <Circle size={10}  className="mr-2 fill-white"/>
+      Investiemento
+    </Badge>)
+}
+
+export default TransactionTypeBadge;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,6 +1,23 @@
-const TransactionsPage = () => {
+import { ArrowDownUp } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns";
+
+
+const TransactionsPage = async () => {
+    const transactions = await db.transaction.findMany({ })
     return ( 
-        <h1>Transaction Page</h1>
+        <div className="p-6 space-y-6">
+            <div className="flex w-full items-center justify-between">
+                <h1 className="text-2xl font-bold">Transações</h1>
+                <Button className="rounded-full">
+                    Adicionar transações
+                    <ArrowDownUp />
+                </Button>
+            </div>
+            <DataTable columns={TransactionColumns} data={transactions} />
+        </div>
     );
 }
  
