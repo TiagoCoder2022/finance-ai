c0724ec9f68329ba51b49ccc291e8b40e4f665d7
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-12-26 18:57:09 -0300

    feat: add transaction pie chart component

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -1,37 +1,15 @@
 import { PiggyBankIcon, TrendingDownIcon, TrendingUpIcon, WalletIcon } from "lucide-react";
 import SummaryCard from "./summary-card";
-import { db } from "@/app/_lib/prisma";
 
 interface SummaryCardsProps {
     month: string;
+    balance: number;
+    depositsTotal: number;
+    investmentsTotal: number;
+    expensesTotal: number;
 }
-const SummaryCards = async ({month}: SummaryCardsProps) => {
-        const where = {
-            date: {
-                gte: new Date(`2024-${month}-01`),
-                lt: new Date(`2024-${month}-31`)
-            }
-        }
-        const depositTotal = Number(
-            (await db.transaction.aggregate({
-            where: { ...where, type: "DEPOSIT" },
-            _sum: { amount: true }
-        })
-    )._sum?.amount)
-        const investmentsTotal = Number(
-            (await db.transaction.aggregate({
-            where: { ...where, type: "INVESTMENT" },
-            _sum: { amount: true }
-        })
-    )._sum?.amount)
-        const expensesTotal = Number(
-            (await db.transaction.aggregate({
-            where: { ...where, type: "EXPENSE" },
-            _sum: { amount: true }
-        })
-    )._sum?.amount)
-
-    const balance = depositTotal - investmentsTotal - expensesTotal
+const SummaryCards = async ({ balance, depositsTotal, investmentsTotal, expensesTotal}: SummaryCardsProps) => {
+       
     return ( 
         <div className="space-y-6">            
             <SummaryCard 
@@ -51,7 +29,7 @@ const SummaryCards = async ({month}: SummaryCardsProps) => {
                 <SummaryCard
                     icon={<TrendingUpIcon size={16} className="text-primary"/>}
                     title="Receita"
-                    amount={depositTotal}
+                    amount={depositsTotal}
                 />              
                 
                 <SummaryCard
