676dd7eae65d846c0a6b4b9a7a20489b5e68c0e3
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-12-29 18:29:53 -0300

    feat: add stripe webhook that hadles subscription

--- app/subscription/_component/acquire-plan-button.tsx
+++ app/subscription/_component/acquire-plan-button.tsx
@@ -3,8 +3,11 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js"
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
+    const {user} = useUser()
     const handleAcquirePlanClick = async () => {
         const { sessionId } = await createStripeCheckout()
 
@@ -18,12 +21,27 @@ const AcquirePlanButton = () => {
         }
         await stripe.redirectToCheckout({ sessionId })
     }
+
+    const hasPrimiumPlan = user?.publicMetadata.subscriptionPlan === 'premium'
+
+    if (hasPrimiumPlan) {
+        return (
+            <Button 
+                className="w-full rounded-full font-bold"               
+                variant="link"
+            >
+                <Link href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}>
+                    Grenciar Plano
+                </Link>
+            </Button>
+        )
+    }
     return ( 
         <Button 
             className="w-full rounded-full font-bold"
-            onClick={handleAcquirePlanClick}
+            onClick={handleAcquirePlanClick}            
         >
-            Adiquirir plano
+            Adiquirir Plano
         </Button>
      );
 }
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,16 +1,20 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_component/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
     const { userId } = await auth()
     
     if (!userId) {
         redirect("/login")
-    }    
+    }
+    
+    const user = await clerkClient().users.getUser(userId)
+    const hasPrimiumPlan = user?.publicMetadata.subscriptionPlan === 'premium' 
     return ( 
         <>
             <Navbar />
@@ -18,8 +22,11 @@ const SubscriptionPage = async () => {
                 <h1 className="font-bold text-2xl">Assinatura</h1>
 
                 <div className="flex gap-6">
-                    <Card className="w-[450px]">
-                        <CardHeader className="border-b border-solid py-8">
+                    <Card className="w-[450px]">                        
+                        <CardHeader className="border-b border-solid py-8 relative">
+                            {!hasPrimiumPlan && (
+                                <Badge className="absolute top-12 left-4 bg-primary/10 text-primary">Ativo</Badge>
+                            )}                            
                             <h2 className="text-center text-2xl font-semibold">Plano Básico</h2>
                             <div className="flex items-center gap-3 justify-center">
                                 <span className="text-4xl">R$</span>
@@ -39,7 +46,10 @@ const SubscriptionPage = async () => {
                         </CardContent>
                     </Card>
                     <Card className="w-[450px]">
-                        <CardHeader className="border-b border-solid py-8">
+                        <CardHeader className="border-b border-solid py-8 relative">
+                            {hasPrimiumPlan && (
+                                <Badge className="absolute top-12 left-4 bg-primary/10 text-primary">Ativo</Badge>
+                            )}
                             <h2 className="text-center text-2xl font-semibold">Plano Primium</h2>
                             <div className="flex items-center gap-3 justify-center">
                                 <span className="text-4xl">R$</span>
